40dec754402513db40d343829867f394b0c76549
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-04-13 08:44:24 +0100

    updated portfolio

--- app/components/site/Navbar.tsx
+++ app/components/site/Navbar.tsx
@@ -131,7 +131,7 @@ const Navbar = () => {
               </motion.li>
             </Link>
           </ul>
-          <a href="https://flowcv.com/resume/ak4qv6c85v" target="_blank">
+          <a href="https://drive.google.com/file/d/129Xm2vE-JqqhgGMMo4McbG4M0JymzWOd/view?usp=sharing" target="_blank">
             <motion.button
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
