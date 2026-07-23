3c78f51f2504ddbe6800d9fd34dfff86992764e8
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-07-23 18:59:43 +0100

    updated projects section

--- app/components/site/Projects.tsx
+++ app/components/site/Projects.tsx
@@ -15,66 +15,8 @@ const Projects = () => {
 
       <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
 
-        {/* ============ project One Start here ================ */}
-        <div className="flex flex-col xl:flex-row gap-6">
-          <a
-            className="w-full xl:w-1/2 h-auto relative group"
-            href="https://durodolaabdulhad.vercel.app/"
-            target="_blank"
-          >
-            <div>
-              <Image
-                className="w-full h-full object-contain"
-                src={"/rasheed-tolulope-project-portfolio.png"}
-                alt="rasheed-tolulope-project-portfolio"
-                fill
-              />
-              <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
-            </div>
-          </a>
-
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
-            <div>
-              <p className="font-titleFont text-textGreen text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">Digial Marketer Portfolio</h3>
-            </div>
-
-            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
-              A portfolio that showcases strong design expertise and a refined visual perspective on a professional journey. It uses <span className="text-textGreen">Sanity</span> as the backend API.
-            </p>
-
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>Nextjs</li>
-              <li>Typescript</li>
-              <li>Next-auth</li>
-              <li>Sanity</li>
-              <li>Vercel Deployment</li>
-            </ul>
-
-            <div className="text-2xl flex gap-4">
-              {/* <a
-                className="hover:text-textGreen duration-300"
-                href="https://github.com/Rasheed124/duroappp"
-                target="_blank"
-              >
-                <TbBrandGithub />
-              </a> */}
-              <a
-                className="hover:text-textGreen duration-300"
-                href="https://durodolaabdulhad.vercel.app/"
-                target="_blank"
-              >
-                <RxOpenInNewWindow />
-              </a>
-            </div>
-          </div>
-        </div>
-        {/* ============ project One End here ================== */}
-
-
-        {/* ============ project Two Start here (Tech Design Space) ================ */}
+  
+        {/* ============ project One Start here (Tech Design Space) ================ */}
         <div className="flex flex-col xl:flex-row-reverse gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
@@ -129,10 +71,10 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        {/* ============ project Two End here ================== */}
+        {/* ============ project One End here ================== */}
 
 
-        {/* ============ project Three Start here ============== */}
+        {/* ============ project Two Start here ============== */}
         <div className="flex flex-col xl:flex-row gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
@@ -186,7 +128,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        {/* ============ project Three End here ================== */}
+        {/* ============ project Two End here ================== */}
 
       </div>
     </section>
